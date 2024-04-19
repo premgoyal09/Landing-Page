872927c53424c91260ca0ca3b7955e4f8953ed14
premgoyal09 <163977408+premgoyal09@users.noreply.github.com>
2024-04-19 22:24:54 +0530

    Add files via upload

---
+++ Landing page  77/LANDINGPAGE.js
@@ -0,0 +1,39 @@
+var tl = gsap.timeline({
+    repeat: -1
+});
+
+
+
+tl.to(".imagecontainer", {
+        ease: Expo.easeInOut,
+        width: "100%",
+        duration: 2,
+        stagger: 2,
+    }, 'a')
+
+// to(".text h1", {
+//     ease: Expo.easeInOut,
+//     stagger: 2,
+//     top: 0,
+// },)
+
+// .to(".text h1", {
+//     delay: 2,
+//     ease: Expo.easeInOut,
+//     stagger: 2,
+//     top: "-100%"
+// },)
+
+
+gsap.to(".text h1", {
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top:0
+})
+
+gsap.to(".text h1",{
+    delay: 2,
+    ease :Expo.easeInOut,
+    stagger: 2,
+    top: "-100%"
+})
